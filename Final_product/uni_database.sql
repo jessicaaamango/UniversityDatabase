ce3e37b12df649b3fadd5708d6c3b828b4365a0e
Jessica Benitez <101780073+jessicaaamango@users.noreply.github.com>
2024-05-14 17:33:59 -0400

    fixing uni_database.sql

--- Final_product/uni_database.sql
+++ Final_product/uni_database.sql
@@ -39,47 +39,6 @@ CREATE TABLE Major (
   FOREIGN KEY (uni_id) REFERENCES university(uni_id),
   FOREIGN KEY (major_id) REFERENCES uni_major(id)
  );
-CREATE TABLE university (
-   uni_name VARCHAR(100),
-   uni_id int PRIMARY KEY,
-   tuition DECIMAL(8,2),
-   state VARCHAR(2)
- );
-
-CREATE TABLE MD_HBCU (
-    uni_id int PRIMARY KEY,
-    uni_name VARCHAR(100),
-    uni_code VARCHAR(10),
-    city VARCHAR(100),
-  FOREIGN KEY (uni_id) REFERENCES university(uni_id)
-);
-
-CREATE TABLE DC_HBCU (
-  uni_id int PRIMARY KEY,
-  uni_name VARCHAR(100),
-  uni_code VARCHAR(10),
-  city VARCHAR(100),
-  FOREIGN KEY (uni_id) REFERENCES university(uni_id)
-);
-
-CREATE TABLE VA_HBCU (
-  uni_id  int PRIMARY KEY,
-  uni_name VARCHAR(100),
-  uni_code VARCHAR(10),
-  city VARCHAR(100),
-  FOREIGN KEY (uni_id) REFERENCES university(uni_id)
-);
-CREATE TABLE uni_major(
-  id INT PRIMARY KEY,
-  name VARCHAR(100)
-);
-CREATE TABLE Major (
-  uni_id int ,
-  major_id int, 
-  major varchar(100),
-  FOREIGN KEY (uni_id) REFERENCES university(uni_id),
-  FOREIGN KEY (major_id) REFERENCES uni_major(id)
- );
 
 INSERT INTO university (uni_name, uni_id, tuition, state)
 VALUES 
